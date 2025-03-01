eb5048eddb6d2dc6b5073c5e4dfc5a7feeb256d4
Imam Hasan <imamets3@gmail.com>
2025-03-01 19:48:43 +0600

    addes massage funtion in right

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const getTaskNum = document.getElementById('task-num');
 const headerNum = document.getElementById('header-num')
-
+;
 
 document.getElementById('completed-btn')
 .addEventListener("click",function(){
@@ -12,8 +12,26 @@ document.getElementById('completed-btn')
   let headerNumChange = parseInt(headerNum.innerText);
   headerNum.innerText = headerNumChange + 1;
 
+  const para = document.createElement('p');
+   para.id = "para-id";
+
+  document.getElementById("sidebar").appendChild(para);
+  showMassageWithTime();
+
 })
 
+// click event show massage
+
+function showMassageWithTime(){
+  const real = new Date();
+  const hours = real.getHours();
+  const minutes = real.getMinutes();
+  const second = real.getSeconds();
+
+  const showMassage = hours + ":" + minutes + ":" + second;
+  document.getElementById("para-id").innerText = "You have Complete The Task Add Dark Mode at" + showMassage;
+  
+}
 
 
 
